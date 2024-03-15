33006e4f6972899c3f0d7be1dbd35e993a7341bc
destro <irtizasafdar@hotmail.co.uk>
2024-03-15 18:00:08 +0000

    fetched job data from linkedin api - api.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import NavBar from "./components/NavBar/navbar.jsx";
 import styled from "styled-components";
 import Wave from "./components/Wave/Wave.jsx";
+import FindJob from "./pages/FindaJob.jsx";
 import Home from "./pages/Home.jsx";
 const AppContainer = styled.div`
   max-width: 1200px;
@@ -14,6 +15,7 @@ const App = () => {
     <AppContainer>
       <NavBar />
       {/* <Wave /> */}
+      <FindJob />
       <Home />
       <CreatePost />
     </AppContainer>
