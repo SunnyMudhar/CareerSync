0100858647071533c7613771e5a5238453d05ada
Sunny <sunielmudhar@gmail.com>
2024-03-20 05:34:20 +0000

    set foldername to lowercase to match github

--- src/pages/FindaJob.jsx
+++ src/pages/FindaJob.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Container from '../components/Container';
 import Row from '../components/Row';
-import JobSearchForm from '../components/JobSearchForm';
+import JobSearchForm from 'src/components/jobSearchForm';
 import JobCard from '../components/JobCard';
 import API from '../utils/API.js';
 
